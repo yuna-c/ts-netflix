6a20f57a2e9b05a19faedd5300ffd6c39c14b8e3
tubi55 <tubi55@nate.com>
2024-01-26 18:14:24 +0900

    tailwind에 특정 tag명이나 클래스명으로 style preset지정가능

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import Header from '@/components/Header';
+
+const Home: NextPage = () => {
   return (
-    <main>
-      <h1>Netflix</h1>
-    </main>
+    <div>
+      <Head>
+        <title>NETFLIX</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main>
+        <h1 className="">Main Page</h1>
+      </main>
+    </div>
   );
-}
+};
+
+export default Home;
