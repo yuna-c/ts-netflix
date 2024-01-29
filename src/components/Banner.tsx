1e3eefa7e2d37b7e143db0bf104aa30c43d42ef8
tubi55 <tubi55@nate.com>
2024-01-29 14:43:58 +0900

    useRef에 유니온타입을 지정하지 않더라도 초기값이 자동으로 generic을 통해서 내부적으로 union 타입 등록됨

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,7 +9,11 @@ interface Props {
 }
 
 const Banner: NextPage<Props> = ({ original }: Props) => {
-	const loading = useRef<any>(null);
+	//useRef에는 초기값이 없을수가 없으므로 직접 값을 수동으로 지정해야 되기 때문
+	//useState와 다르게 useRef는 unionType을 지정하지 않더라도 인수로 지정한 초기값을 자동으로 유니온타입 설정
+	const loading = useRef<HTMLDivElement>(null);
+	//useState는 초기값을 집어넣지 않더라도 추후담기는 값을 인지해서 타입추론
+	//useState는 예외사항에 대한 값을 무조건 유니온타입으로 지정해야함
 	const [Movie, setMovie] = useState<Movie | null>(null);
 	console.log(Movie);
 
@@ -19,7 +23,7 @@ const Banner: NextPage<Props> = ({ original }: Props) => {
 	}, [original]);
 
 	return (
-		<section className='h-screen px-4 pb-20 pt-40 flex flex-col space-y-4'>
+		<section className='flex flex-col h-screen px-4 pt-40 pb-20 space-y-4 '>
 			{Movie && (
 				<>
 					{/* pic Frame */}
@@ -32,7 +36,7 @@ const Banner: NextPage<Props> = ({ original }: Props) => {
 							quality={70}
 							sizes='(max-width:768px) 100vw, (max-width:1200) 70vw, 100vw'
 							className='object-cover'
-							onLoadingComplete={() => loading.current.remove()}
+							onLoadingComplete={() => loading.current?.remove()}
 						/>
 					</div>
 
