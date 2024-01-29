06f2ef1963d176a0919f4e4bef8dab568390bedb
tubi55 <tubi55@nate.com>
2024-01-29 13:04:33 +0900

    tailwind config로 미리 atimation구문을 animate preset에 등록후 Image컴포넌트에 로딩바 보이도록 처리

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Movie } from '../../types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { baseURL } from '@/url';
 import type { NextPage } from 'next';
 
@@ -9,6 +9,7 @@ interface Props {
 }
 
 const Banner: NextPage<Props> = ({ original }: Props) => {
+	const loading = useRef<any>(null);
 	const [Movie, setMovie] = useState<Movie | null>(null);
 	console.log(Movie);
 
@@ -31,11 +32,17 @@ const Banner: NextPage<Props> = ({ original }: Props) => {
 							quality={70}
 							sizes='(max-width:768px) 100vw, (max-width:1200) 70vw, 100vw'
 							className='object-cover'
+							onLoadingComplete={() => loading.current.remove()}
 						/>
 					</div>
 
 					{/* gradient layer */}
 					<div className='absolute top-0 left-0 w-full h-full z-[2] bg-gradient1'></div>
+
+					{/* loading bar */}
+					<div
+						ref={loading}
+						className='w-[40px] h-[40px] absolute top-[50%] left-[50%] translate-x-[-50%] trasnlate-y-[-50%] border-4 border-[orange] border-t-[transparent] rounded-[50%] z-[3] animate-ani-rotation'></div>
 
 					{/* title */}
 					<h1 className='relative z-[3] text-2xl font-bold md:text-4xl lg:text-7xl'>{Movie?.title || Movie?.name}</h1>
