19d7e9ed3580a308954603e6d0ab736b721c9c39
yuna <yuna744@gmail.com>
2024-01-30 13:06:08 +0900

    0130 커스텀훅 안에서 로그인 정보객체를 받아서 전역 context에 전달해주는 Provider컴포넌트 생성

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,34 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from 'firebase/auth';
+import { useRouter } from 'next/router';
+import React, { createContext, useContext, useEffect, useState, useMemo } from 'react';
+import { auth } from '@/firebase';
+
+//전역 Context에 전달할 인증정보 타입
+interface IAuth {
+	UserInfo: User | null;
+	signIn: (email: string, password: string) => Promise<void>;
+	signUp: (email: string, password: string) => Promise<void>;
+	signOut: () => Promise<void>;
+	Errors: string | null;
+	Loading: boolean;
+}
+
+//전역State provider에 전달할 Props 타입
+interface AuthProviderProps {
+	children: React.ReactNode;
+}
+
+//firebase로부터 전달받아서 전역Context에 전달할 객체모음
+const AuthContext = createContext<IAuth>({
+	UserInfo: null,
+	signUp: async () => {},
+	signIn: async () => {},
+	signOut: async () => {},
+	Errors: null,
+	Loading: false
+});
+
+//전체 컴포넌트를 감싸줄 Wrapping컴포넌트 (전역데이터를 모든 컴포넌트에 전달하는 최상위 부모 컴포넌트)
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+	return <AuthContext.Provider value={{ UserInfo, signIn, signOut, signUp, Loading, Errors }}>{children}</AuthContext.Provider>;
+};
